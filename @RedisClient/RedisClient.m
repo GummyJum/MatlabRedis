3098c9dbaac6c6931d4bbe55a4e83512c1008f9c
GumJum <gumjum.o.o@gmail.com>
2021-04-13 10:08:14 +0300

    Update RedisClient.m

--- @RedisClient/RedisClient.m
+++ @RedisClient/RedisClient.m
@@ -8,6 +8,7 @@ classdef RedisClient < handle
        recv_buffer = []
        timeout = 2
        buffer_wait = 0.001
+       CRNL = sprintf('\r\n')
    end
    methods
         function obj = RedisClient(host, port, varargin)
@@ -28,7 +29,7 @@ classdef RedisClient < handle
             end
         end
         function r = ping(obj)
-            r = obj.send('ping');
+            r = obj.send('PING');
         end
         
         function dump_recv_buffer(obj)
@@ -49,31 +50,75 @@ classdef RedisClient < handle
         end        
         
         function send_async(obj, varargin)
-            crnl = sprintf('\r\n');
             buff = sprintf('*%d\r\n', numel(varargin));
             args = cellfun(@(x) {[sprintf('$%d\r\n', numel(x)), x]}, varargin);
-            buff = [buff, strjoin(args, crnl), crnl];
-            fprintf('sending:\r\n%s', buff);
+            buff = [buff, strjoin(args, obj.CRNL), obj.CRNL];
             obj.socket.write(uint8(buff));            
         end
-        
-        function r = recv_async(obj, varargin)
+               
+        function [r, status] = recv_async(obj, varargin)
             % we have 5 possible responses first char +-:$*
-            obj.dump_recv_buffer;
+            r = [];
+            status = 0;
+            if isempty(obj.recv_buffer)
+                obj.dump_recv_buffer;
+            end
+            
+            lines = strsplit(obj.recv_buffer, obj.CRNL);
+            
+            while ~isempty(lines) && isempty(lines{1})
+                lines = lines(2:end);
+            end
+            if isempty(lines)
+                return;
+            end
+            
+            if any(lines{1}(1) == '+-:')
+                r = lines{1}(2:end);
+                if lines{1}(1) == '-'
+                    status = 1;
+                end
+                if lines{1}(1) == ':'
+                    r = str2double(r);
+                end
+                obj.recv_buffer = strjoin(lines(2:end), obj.CRNL);
+                return
+            end
+            
+            if lines{1}(1) == '$'
+                assert(length(lines) >= 2, 'data was not received as expected');
+                r = lines{2};
+                obj.recv_buffer = strjoin(lines(3:end), obj.CRNL);
+                return
+            end
+            
+            if lines{1}(1) == '*'
+                n = str2double(lines{1}(2:end));
+                obj.recv_buffer = strjoin(lines(2:end), obj.CRNL);
+                r = cell(1, n);
+                for ind = 1:n
+                    r{ind} = obj.recv_async;
+                end
+                return
+            end
+                
+            status = 1;
             r = obj.recv_buffer;
-            obj.recv_buffer = [];
-            % we should now have
-            fprintf('received:\r\n%s', r);            
+            obj.recv_buffer = [];   
+            warning('could not parse RESP return raw response');
         end
-        function r = send(obj, varargin)
+        
+        function [r, status] = send(obj, varargin)
             obj.send_async(varargin{:});
-            r = obj.recv_async(varargin{:});
+            [r, status] = obj.recv_async(varargin{:});
         end
+        
         function r = set(obj, varargin)
-            r = obj.send('set', varargin);
+            r = obj.send('SET', varargin);
         end
+        
         function r = get(obj, varargin)
-            r = obj.send('get', varargin);
+            r = obj.send('GET', varargin);
         end
    end
 end
